101d6a3aaf4741c95d77d373cbbbad3c74cae4dc
Patrick Sinkuts <s.patrick.jp@gmail.com>
2021-07-20 14:32:54 +0200

    Client Chat

---
+++ Client/Client/Client.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+
+#pragma warning(disable: 4996)
+using namespace std;
+
+SOCKET Connection;
+
+void ClientHandler() {
+	char msg[256];
+	while (true) {
+		recv(Connection, msg, sizeof(msg), NULL);
+		cout << msg << endl;
+	}
+}
+
+int main(int argc, char* argv()) {
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET; // семество интернет протоколов
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+	
+		cout << "Error" << endl;
+		return 1;
+	}
+	else {
+		cout << "COnnection" << endl;
+	}
+
+
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, NULL, NULL, NULL);
+
+	char msg1[256];
+	while (true) {
+		cin.getline(msg1, sizeof(msg1));
+		send(Connection, msg1, sizeof(msg1), NULL);
+		Sleep(10);
+	}
+
+	system("pause");
+	return 0;
+}
